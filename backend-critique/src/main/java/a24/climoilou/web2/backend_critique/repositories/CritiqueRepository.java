4f42e75a93c2507e9ea66939184ca75b4c01ebc6
Janelle <2264502@cegeplimoilou.ca>
2024-11-13 17:00:17 -0500

    Créer CritiqueRepository #5

---
+++ backend-critique/src/main/java/a24/climoilou/web2/backend_critique/repositories/CritiqueRepository.java
@@ -0,0 +1,7 @@
+package a24.climoilou.web2.backend_critique.repositories;
+
+import a24.climoilou.web2.backend_critique.models.Critique;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CritiqueRepository extends CrudRepository<Critique, Long> {
+}
