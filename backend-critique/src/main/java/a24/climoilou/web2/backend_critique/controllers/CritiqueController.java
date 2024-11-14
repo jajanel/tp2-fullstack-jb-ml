0e634d272f370f5d4aabcfc03ac547541fe85f89
JanelleB2 <2264502@cegeplimoilou.ca>
2024-11-14 11:43:47 -0500

    Ajout des fonction de base pour controller #8

--- backend-critique/src/main/java/a24/climoilou/web2/backend_critique/controllers/CritiqueController.java
+++ backend-critique/src/main/java/a24/climoilou/web2/backend_critique/controllers/CritiqueController.java
@@ -1,4 +1,39 @@
 package a24.climoilou.web2.backend_critique.controllers;
 
+import a24.climoilou.web2.backend_critique.models.Critique;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
 public class CritiqueController {
+
+    @GetMapping("/critiques")
+    public Collection<Critique> getAllCritiques(){
+
+        return null;
+    }
+
+    @GetMapping("/critiques/{id}")
+    public Collection<Critique> getCritiqueParBirdId(@PathVariable Long id){
+
+        return null;
+    }
+
+    @PostMapping("/ajouterCritique")
+    public void ajouterCritique(@RequestBody Critique critique){
+
+    }
+
+    @DeleteMapping("/supprimerCritique/{id}")
+    public void supprimerCritique(@PathVariable Long id){
+
+    }
+
+   @PatchMapping("/modifierCritique/{id}")
+    public void modifierCritique(@PathVariable Long id, @RequestBody Critique critique){
+
+    }
+
+
 }
