5ad35d69c3f2ea8d288e52732e0fea6689d413da
Janelle <2264502@cegeplimoilou.ca>
2024-11-20 16:33:17 -0500

    Ajout critique validateur #13

--- backend-critique/src/main/java/a24/climoilou/web2/backend_critique/BackendCritiqueApplication.java
+++ backend-critique/src/main/java/a24/climoilou/web2/backend_critique/BackendCritiqueApplication.java
@@ -3,12 +3,14 @@ package a24.climoilou.web2.backend_critique;
 import a24.climoilou.web2.backend_critique.controllers.CritiqueController;
 import a24.climoilou.web2.backend_critique.models.Critique;
 import a24.climoilou.web2.backend_critique.repositories.CritiqueRepository;
+import a24.climoilou.web2.backend_critique.validators.CritiqueValidateur;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -28,18 +30,12 @@ public class BackendCritiqueApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("******* Initialisation des critiques par défaut *******");
-		//POur l'initialisation des critiques par défaut dans notre bd
 
 
-		logger.info("******* Création d'instances de critiques *******");
+	}
 
-
-		logger.info("******* Ajout des critiques à la base de donnée *******");
-
-
-		logger.info("******* Fin de l'initialisation des critiques par défaut *******");
-
-
+	@Bean
+	CritiqueValidateur getCritiqueValidateur(){
+		return new CritiqueValidateur();
 	}
 }
