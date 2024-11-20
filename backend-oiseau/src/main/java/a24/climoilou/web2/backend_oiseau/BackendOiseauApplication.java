1631765e35a433af9a2dc48365852e5b6689b366
Maek Lorman <157525653+Alakazouu@users.noreply.github.com>
2024-11-20 16:41:01 -0500

    Ajout de méthodes dans le controlleur #10
    j'ai aussi modifier un peu l'application backend de oiseau

--- backend-oiseau/src/main/java/a24/climoilou/web2/backend_oiseau/BackendOiseauApplication.java
+++ backend-oiseau/src/main/java/a24/climoilou/web2/backend_oiseau/BackendOiseauApplication.java
@@ -1,10 +1,36 @@
 package a24.climoilou.web2.backend_oiseau;
 
+import a24.climoilou.web2.backend_oiseau.Models.Oiseau;
+import a24.climoilou.web2.backend_oiseau.Repositories.OiseauRepository;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class BackendOiseauApplication {
+
+	@Autowired
+	private static OiseauRepository oiseauRepository;
+
+	@Bean
+	CommandLineRunner initData() {
+		return this::run;
+	}
+
+	private void run(String... strings) {
+		Logger logger = (Logger) LoggerFactory.getLogger(BackendOiseauApplication.class);
+		logger.info("init Data*********************************");
+
+		oiseauRepository.save(new Oiseau("poule", "ouiseau", 2, "Asie", new Date(), "aucuneSrc"));
+		oiseauRepository.findAll().forEach(System.out::println);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendOiseauApplication.class, args);
--- backend-oiseau/src/main/java/a24/climoilou/web2/backend_oiseau/Controllers/OiseauControlleur.java
+++ backend-oiseau/src/main/java/a24/climoilou/web2/backend_oiseau/Controllers/OiseauControlleur.java
@@ -1,18 +1,48 @@
 package a24.climoilou.web2.backend_oiseau.Controllers;
 
+import a24.climoilou.web2.backend_oiseau.Exceptions.OiseauNotFoundException;
 import a24.climoilou.web2.backend_oiseau.Models.Oiseau;
-import a24.climoilou.web2.backend_oiseau.Models.Oiseau;
+import a24.climoilou.web2.backend_oiseau.Repositories.OiseauRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.logging.Logger;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class OiseauControlleur {
 
-    @GetMapping("/oiseaux")
-    public Collection<Oiseau> getAllOiseaux(){
+    private final OiseauRepository oiseauRepository;
+    private Logger logger = (Logger) LoggerFactory.getLogger(OiseauControlleur.class);
 
-        return null;
+    public OiseauControlleur(OiseauRepository oiseauRepository) {
+        this.oiseauRepository = oiseauRepository;
+    }
+
+    @GetMapping(value = "/oiseaux", produces = "application/json")
+    public String getAllOiseaux(){
+
+        return """ 
+                    [{
+                    "idOiseau": 1,
+                    "categorie": "poule",
+                    "race": "chantecler",
+                    "prix": 50,
+                    "origine": "amerique",
+                    "datePublication": "2024-12-12",
+                    "srcImage": "src/assets/images/poules/chantecler.jpg"
+                    },
+                    {
+                    "idOiseau": 2,
+                    "categorie": "poule",
+                    "race": "ayam cemami",
+                    "prix": 95,
+                    "origine": "asie",
+                    "datePublication": "2024-12-02",
+                    "srcImage": "src/assets/images/poules/ayam.jpg"
+                    }]
+               """;
     }
 
     @GetMapping("/oiseaux/{id}")
@@ -22,13 +52,27 @@ public class OiseauControlleur {
     }
 
     @PostMapping("/ajouterOiseau")
-    public void ajouterOiseau(@RequestBody Oiseau oiseau){
+    public Long ajouterOiseau(@RequestBody Oiseau oiseau){
+        logger.info("ajout de l'oiseau " + oiseau);
+        Long id = -1L;
 
+        id = oiseauRepository.save(oiseau).getId();
+        logger.info("le nouvel id est : " + id);
+
+        return id;
     }
 
     @DeleteMapping("/supprimerOiseau/{id}")
     public void supprimerOiseau(@PathVariable Long id){
+        logger.info("deleteOiseau " + id);
 
+        if (oiseauRepository.existsById(id)) {
+            oiseauRepository.deleteById(id);
+
+        } else {
+            logger.warning("le id demandé n'existe pas");
+            throw new OiseauNotFoundException();
+        }
     }
 
     @PatchMapping("/modifierOiseau/{id}")
