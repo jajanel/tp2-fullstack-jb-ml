a37b0f8f3bc133498f9ddd3761bfbf29d042f8f8
Janelle <2264502@cegeplimoilou.ca>
2024-11-22 16:56:54 -0500

    Début des fonction d'appel du serv #23 #

---
+++ tp1-react-jb-ml-master/src/scripts/http-critiques.js
@@ -0,0 +1,56 @@
+/**
+ * Appel la fonction qui retourne toutes les critiques dans la BD
+ */
+export async function fetchAllCritiques(){
+    const response = await fetch("http://localhost:8080/critiques")
+    if (!response.ok){
+        throw new Error ("Erreur du chargement de toutes les critiques. Pour: "+ await response.json())
+    }
+    return await response.json()
+}
+
+/**
+ * Fetch toutes les critiques pour un oiseau
+ * @param nomOiseau l'oiseau pour lequel on veux les critiques
+ */
+export async function fetchCritiqueParOiseau(nomOiseau){
+    const response = await fetch("http://localhost:8080/critiques/" + nomOiseau)
+    if (!response.ok){
+        throw new Error ("Erreur du chargement de la critique de l'oiseau: " + nomOiseau + " pour: " + await response.json())
+    }
+    return await response.json()
+
+}
+
+/**
+ * Fonction qui appel la méthode d'ajout de critique en BD
+ * @param critique la critique passée en paramètre à ajouter
+ * @returns {Promise<any>} la réponse json
+ */
+export async function ajouterCritique(critique){
+    const response = await fetch("http://localhost:8080/ajouterCritique/" + critique,
+        {
+            method: 'POST',
+            body: JSON.stringify(critique),
+            headers: {'Content-type': 'application/json'}
+        })
+        if (!response.ok) throw new Error("Erreur d'ajout de la nouvelle critique. Pour: " + await response.json())
+
+    return await response.json()
+}
+
+/**
+ * Méthode qui permet d'appeller la méthode de suppression de la critique en BD
+ * @param idCritique l'id de la crtiique que l'on veut supprimer
+ * @returns {Promise<any>} le message de la méthode de suppression json
+ */
+export async function supprimerCritique(idCritique){
+    const response = await fetch("http://localhost:8080/supprimerCritique/" + idCritique,
+        {
+            method: 'DELETE',
+        })
+    if (!response.ok) throw new Error("La critique n'a pas pu être supprimée")
+    return await response.json()
+}
+
+
