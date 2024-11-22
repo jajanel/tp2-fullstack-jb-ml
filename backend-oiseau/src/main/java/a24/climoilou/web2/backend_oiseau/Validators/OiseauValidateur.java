b4439dc1ee778c44682a1df9316c9b39cc218879
Maek Lorman <157525653+Alakazouu@users.noreply.github.com>
2024-11-22 15:43:11 -0500

    Ajout de validations pour oiseau #12

--- backend-oiseau/src/main/java/a24/climoilou/web2/backend_oiseau/Validators/OiseauValidateur.java
+++ backend-oiseau/src/main/java/a24/climoilou/web2/backend_oiseau/Validators/OiseauValidateur.java
@@ -1,4 +1,34 @@
 package a24.climoilou.web2.backend_oiseau.Validators;
 
 public class OiseauValidateur {
+
+    public String validateCategorie(String categorie) {
+        return categorie != null && (
+                categorie == "poule" ||
+                categorie == "oie" ||
+                categorie == "canard" ||
+                categorie == "dinde") ? "" : "Categorie invalide : veuillez choisir une categorie valide (poule, oie, canard ou dinde)\n";
+    }
+
+    public String validateOrigine(String origine) {
+        return origine != null && (
+                origine == "amerique" ||
+                        origine == "asie" ||
+                        origine == "europe" ||
+                        origine == "afrique" ||
+                        origine == "oceanie") ? "" : "Origine invalide : veuillez choisir un continent valide (amerique, asie, europe, afrique ou oceanie)\n";
+    }
+
+    public String validateRace(String race) {
+        return race != null ? "" : "race invalide\n";
+    }
+
+    public String validatePrix(double prix) {
+        return prix > 0 ? "" : "prix invalide\n";
+    }
+
+    public String validateSrc(String src) {
+        return src != null &&
+                src.startsWith("src/assets/images/") ? "" : "src invalide : veuillez mettre une source qui commence par \"src/assets/images/\"\n";
+    }
 }
