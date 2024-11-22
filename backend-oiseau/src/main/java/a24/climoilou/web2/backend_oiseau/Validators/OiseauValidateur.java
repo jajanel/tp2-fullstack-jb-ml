c9f4db4acce14679a4f1bda52e38df9e464d97f0
Maek Lorman <157525653+Alakazouu@users.noreply.github.com>
2024-11-22 16:32:31 -0500

    Ajout de validationOiseau #12

--- backend-oiseau/src/main/java/a24/climoilou/web2/backend_oiseau/Validators/OiseauValidateur.java
+++ backend-oiseau/src/main/java/a24/climoilou/web2/backend_oiseau/Validators/OiseauValidateur.java
@@ -1,22 +1,34 @@
 package a24.climoilou.web2.backend_oiseau.Validators;
 
+import a24.climoilou.web2.backend_oiseau.Models.Oiseau;
+
 public class OiseauValidateur {
 
-    public String validateCategorie(String categorie) {
-        return categorie != null && (
-                categorie == "poule" ||
-                categorie == "oie" ||
-                categorie == "canard" ||
-                categorie == "dinde") ? "" : "Categorie invalide : veuillez choisir une categorie valide (poule, oie, canard ou dinde)\n";
+    public String validateoiseau(Oiseau oiseau) {
+        return "" +
+                validateCategorie(oiseau.getCategorie()) +
+                validateRace(oiseau.getRace()) +
+                validatePrix(oiseau.getPrix()) +
+                validateOrigine(oiseau.getOrigine()) +
+                validateSrc(oiseau.getSrcImage());
+
     }
 
-    public String validateOrigine(String origine) {
+    public String validateCategorie(Oiseau.Categorie categorie) {
+        return categorie != null && (
+                categorie == Oiseau.Categorie.poule ||
+                categorie == Oiseau.Categorie.oie ||
+                categorie == Oiseau.Categorie.canard ||
+                categorie == Oiseau.Categorie.dinde) ? "" : "Categorie invalide : veuillez choisir une categorie valide (poule, oie, canard ou dinde)\n";
+    }
+
+    public String validateOrigine(Oiseau.Origine origine) {
         return origine != null && (
-                origine == "amerique" ||
-                        origine == "asie" ||
-                        origine == "europe" ||
-                        origine == "afrique" ||
-                        origine == "oceanie") ? "" : "Origine invalide : veuillez choisir un continent valide (amerique, asie, europe, afrique ou oceanie)\n";
+                origine == Oiseau.Origine.amerique ||
+                        origine == Oiseau.Origine.asie ||
+                        origine == Oiseau.Origine.europe ||
+                        origine == Oiseau.Origine.afrique ||
+                        origine == Oiseau.Origine.oceanie) ? "" : "Origine invalide : veuillez choisir un continent valide (amerique, asie, europe, afrique ou oceanie)\n";
     }
 
     public String validateRace(String race) {
