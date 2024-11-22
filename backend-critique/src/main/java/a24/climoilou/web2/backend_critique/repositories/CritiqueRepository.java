7445167e90babccc719722e606c69037c5307177
Janelle <2264502@cegeplimoilou.ca>
2024-11-22 14:55:17 -0500

    Ajout crud find first par Nom Oiseau #8
    Voir commit: 9c31dc76ee1d71c21cf7098ee4c4fbf8144ebc11

--- backend-critique/src/main/java/a24/climoilou/web2/backend_critique/repositories/CritiqueRepository.java
+++ backend-critique/src/main/java/a24/climoilou/web2/backend_critique/repositories/CritiqueRepository.java
@@ -3,5 +3,10 @@ package a24.climoilou.web2.backend_critique.repositories;
 import a24.climoilou.web2.backend_critique.models.Critique;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
+
 public interface CritiqueRepository extends CrudRepository<Critique, Long> {
+
+Collection<Critique> findFirstByNomOiseau(String nomOiseau);
+
 }
