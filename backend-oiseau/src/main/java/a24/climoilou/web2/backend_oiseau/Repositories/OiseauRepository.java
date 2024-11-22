f6419ba7b9a104dcfa9ef7b9666c7ad2c43f9742
Maek Lorman <157525653+Alakazouu@users.noreply.github.com>
2024-11-22 15:16:27 -0500

    Ajout des findOiseau By race et id

--- backend-oiseau/src/main/java/a24/climoilou/web2/backend_oiseau/Repositories/OiseauRepository.java
+++ backend-oiseau/src/main/java/a24/climoilou/web2/backend_oiseau/Repositories/OiseauRepository.java
@@ -4,5 +4,6 @@ import a24.climoilou.web2.backend_oiseau.Models.Oiseau;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OiseauRepository extends CrudRepository <Oiseau, Long> {
-
+    Oiseau findByRace(String race);
+    Oiseau findOiseauById(Long id);
 }
