cd1e3e6107682bf8da99b2863fba816286be8a86
Janelle <2264502@cegeplimoilou.ca>
2024-11-27 17:24:24 -0500

    Fonction inutile dans le controller critique #8

--- backend-critique/src/main/java/a24/climoilou/web2/backend_critique/controllers/CritiqueController.java
+++ backend-critique/src/main/java/a24/climoilou/web2/backend_critique/controllers/CritiqueController.java
@@ -100,29 +100,6 @@ public class CritiqueController {
         throw new CritiqueNotFoundException();
     }
 
-    /**
-     * Fonction qui permet de modifier une critique
-     * @param id l'id de la critique a modifier
-     * @param critiqueModifiee la critique avec ses nouvelles informations passée en paramètres
-     * @return la critique modifiée
-     */
-    @PatchMapping("/modifierCritique/{id}")
-    public Critique modifierCritique(@PathVariable Long id, @RequestBody Critique critiqueModifiee) {
-    Critique critiqueRet = null;
-    if (critiqueValidateur.validateCritiqueComplete(critiqueModifiee)){
-        critiqueRet = critiqueRepository.findById(id)
-                .map(critique1 -> {
-                    critique1.setBeaute(critiqueModifiee.getBeaute());
-                    critique1.setTemperament(critiqueModifiee.getTemperament());
-                    critique1.setUtilisation(critiqueModifiee.getUtilisation());
-                    critique1.setNoteGlobale(critiqueModifiee.getNoteGlobale());
-                    critique1.setRaceOiseau(critiqueModifiee.getRaceOiseau());
-                    critique1.setNomOiseau(critiqueModifiee.getNomOiseau());
-                    return critiqueRepository.save(critiqueModifiee);
-                }).orElseThrow(CritiqueInvalideException::new);
-    }
-    return critiqueRet;
-    }
 
     /**
      * Retourne la note la plus haute de toute la liste de critique
