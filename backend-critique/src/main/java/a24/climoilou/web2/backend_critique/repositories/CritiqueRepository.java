e2b9ae9367cb5e8568edadd5da8ebe96e1398a83
JanelleB2 <2264502@cegeplimoilou.ca>
2024-12-01 17:02:07 -0500

    Query pour calcul note globale #29

--- backend-critique/src/main/java/a24/climoilou/web2/backend_critique/repositories/CritiqueRepository.java
+++ backend-critique/src/main/java/a24/climoilou/web2/backend_critique/repositories/CritiqueRepository.java
@@ -1,14 +1,23 @@
 package a24.climoilou.web2.backend_critique.repositories;
 
 import a24.climoilou.web2.backend_critique.models.Critique;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 
 public interface CritiqueRepository extends CrudRepository<Critique, Long> {
 
-Collection<Critique> findFirstByNomOiseau(String nomOiseau);
+Collection<Critique> findFirstByRaceOiseau(String raceOiseau);
 
-Iterable<Critique> findAllByNomOiseau(String nomOiseau);
+Iterable<Critique> findAllByRaceOiseau (String raceOiseau);
+
+Boolean existsByRaceOiseau(String raceOiseau);
+
+@Query("SELECT (c.temperament + c.beaute + c.utilisation)/3 FROM Critique c WHERE c.id = :id")
+Double calculNoteGlobale(@Param("id") Long id);
+
+
 
 }
