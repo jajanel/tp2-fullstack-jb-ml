5dcf86965561fe52e4d8a11c4b043be4bb07f785
JanelleB2 <2264502@cegeplimoilou.ca>
2024-12-01 18:24:52 -0500

    Affichage critique nombre #38

--- tp1-react-jb-ml-master/src/components/CritiquePrecedente.jsx
+++ tp1-react-jb-ml-master/src/components/CritiquePrecedente.jsx
@@ -1,16 +1,15 @@
-import { supprimerCritique } from '../classes/gestionCatalogueCritique.js';
 
 export default function CritiquePrecedente(props) {
 
     /**
      * Fonction pour supprimer une critique de la liste des critiques, affiche message d'alerte
-     * @param idCritique l'id de la critique à supprimer
-     */
     const handleSupprimerCritique = (idCritique) => {
         supprimerCritique(idCritique);
         props.setDataCritiques(prevCritiques => prevCritiques.filter(critique => critique.idCritique !== idCritique));
         alert("Critique #" + idCritique + " supprimée");
-    };
+    }
+     */
+    //Ici appeller la méthode de suppression de la critique en BD et fetch les autres criutique pour un oiseau
 
     return (
         <div className=" rounded-3 p-2 my-5">
@@ -19,20 +18,20 @@ export default function CritiquePrecedente(props) {
                 <input type="text" className="form-control form-select-lg" id="note" disabled value={props.idCritique} />
             </div>
             <div className="pb-4">
-                <h4 className="form-label">Note globale:</h4>
+                <h4 className="form-label">La note globale calculée:</h4>
                 <input type="number" className="form-control form-select-lg" id="note" disabled value={props.note} />
             </div>
             <div className="pb-4">
                 <h4 className="form-label">Tempérament:</h4>
-                <input type="text" className="form-control form-select-lg" id="temperament" value={props.temperament} disabled />
+                <input type="number" className="form-control form-select-lg" id="temperament" value={props.temperament} disabled />
             </div>
             <div className="pb-4">
                 <h4 className="form-label">Beauté:</h4>
-                <input type="text" className="form-control form-select-lg" id="beaute" disabled value={props.beaute} />
+                <input type="number" className="form-control form-select-lg" id="beaute" disabled value={props.beaute} />
             </div>
             <div className="pb-4">
                 <h4 className="form-label">Utilisation:</h4>
-                <input type="text" className="form-control form-select-lg" id="utilisation" disabled value={props.utilisation} />
+                <input type="number" className="form-control form-select-lg" id="utilisation" disabled value={props.utilisation} />
             </div>
             <hr />
             <div className="btn-wrapper text-center d-flex justify-content-between">
